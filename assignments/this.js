008bbc68865440ae4eae8539ec76847e2fee6635
Ty Lippe <tylippe94@gmail.com>
2019-05-08 17:21:24 -0400

    this.js completed

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding, 'this' will call on the global element when no other rules are met
+* 2. Implicit Binding, 'this' will act as the object and can be called on from inside function to log elements
+* 3. New Binding, 'this' can be put into a generic function with place holders. Outside of function we can place a 'new' to tell the function what elements will be changed to.
+* 4. Explicit Binding, 'this' will take place when we use functions like 'call', 'apply', or 'bind'. Element can be placed outside of function
 *
 * write out a code example of each explanation above
 */
@@ -17,7 +17,7 @@ function sayAge () {
     console.log(`My age is ${this.age}`)
   }
   
-  const user = {
+  const winUser = {
     name: 'Ty',
     age: 24
   }
@@ -30,7 +30,7 @@ function sayAge () {
     console.log(`My age is ${this.age}`)
   }
   
-  const user2 = {
+  const winUser2 = {
     name: 'Ty',
     age: 24
   }
@@ -64,7 +64,27 @@ earth.facts()
 // Principle 3
 
 // code example for New Binding
+function newUser(name, age, location) { //can build a User that can be changed without ever having to touch the function
+    this.name = name
+    this.age = age
+    this.location = location
+    console.log(`${this.name}, ${this.age}, ${this.location}`) 
+}
+  
+  const me = new newUser('Ty', 24, 'Orlando')
+  const wife = new newUser('Dev', 24, 'Orlando')
+ 
 
 // Principle 4
 
 // code example for Explicit Binding
+function sayAge () { //using the .call we can tell the function to use a variable that we can have outside the function
+    console.log(`My age is ${this.age}`)
+  }
+  
+  const exUser = {
+    name: 'Ty',
+    age: 24
+  }
+  
+  sayAge.call(exUser)
